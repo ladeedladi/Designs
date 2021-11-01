15e2747d47178613d2606681f94391d6d103f9ac
ladeed <muhammed.ladeed123@gmail.com>
2021-11-01 12:33:26 +0530

    Add files via upload

---
+++ foldedImage/foldedImage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from "../styles/foldedImage.module.css"
+const FoldedImage = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.innerContainer}>
+               <div></div>
+               <div></div>
+               <div></div>
+               <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default FoldedImage
