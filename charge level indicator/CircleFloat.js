0b18f3fb404a66cd7674199b363144226242cb81
ladeed <muhammed.ladeed123@gmail.com>
2021-11-05 17:37:40 +0530

    Add files via upload

---
+++ charge level indicator/CircleFloat.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "../styles/CircleFloat.module.css"
+const CircleFloat = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.circle}>
+                <h2>83%</h2>
+            </div>
+        </div>
+    )
+}
+
+export default CircleFloat
